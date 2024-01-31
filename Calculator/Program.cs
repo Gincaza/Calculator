4b5765d0264e5c93ebf88635f0eb5f3976a94825
mothnue <dinocruz434@gmail.com>
2024-01-31 10:22:52 +0000

    Adicionando Classe Calculator
    - Implementação da classe Calculator para realizar operações matemáticas básicas.
    - Adicionado método estático DoOperation para executar as operações com dois números e um operador.
    - Operações incluem adição ('a'), subtração ('s'), multiplicação ('m'), e divisão ('d' com verificação de divisão por zero).

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,5 +4,29 @@
 }
 class Calculator
 {
+    public static double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN;
 
+        switch (op)
+        {
+            case "a":
+                result = num1 + num2;
+                break;
+            case "s":
+                result = num1 - num2;
+                break;
+            case "m":
+                result = num1 * num2;
+                break;
+            case "d":
+                //Se preencher zero ele nao aceita
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                break;
+        }
+        return result;
+    }
 }
